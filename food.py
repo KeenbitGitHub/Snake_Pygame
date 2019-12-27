6ecc34e98c88e39b5af362a88ebdb25d2b2f2f1f
KeenBitGitHub <KeenBitInfo@gmail.com>
2019-12-27 23:35:31 +0100

    minor bug fixes; game seems to be working fine

--- food.py
+++ food.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import random
 import pygame
 
@@ -8,8 +8,8 @@ class Food:
     def respawn(self, DONT_SPAWN_ARRAY, WIN_SIZE, BLOCK_WIDTH, BLOCK_HEIGHT):
         x = random.randrange(0, int(WIN_SIZE[0]/BLOCK_WIDTH)) * BLOCK_WIDTH
         y = random.randrange(0, int(WIN_SIZE[1]/BLOCK_HEIGHT)) * BLOCK_HEIGHT
-        self.position = [x, y] 
-        if self.position in DONT_SPAWN_ARRAY:
+        self.position = np.array([x, y])
+        if ((self.position == DONT_SPAWN_ARRAY).all(axis = 1).any()):
             self.respawn(DONT_SPAWN_ARRAY, WIN_SIZE, BLOCK_WIDTH, BLOCK_HEIGHT)
             
     def __init__(self, DONT_SPAWN_ARRAY, WIN_SIZE, BLOCK_WIDTH, BLOCK_HEIGHT):
--- snake.py
+++ snake.py
@@ -15,9 +15,9 @@ class Snake:
         self.alive = True
         self.body = (np.array([start_pos[0] - 1 * BLOCK_SIZE[0], start_pos[1]])).reshape((-1, 2))
         self.head = self.body[0]
-        for i in range(1, snake_len + 1):
+        for i in range(2, snake_len + 2):
             self.body = (np.append(self.body, [start_pos[0] - i * BLOCK_SIZE[0], start_pos[1]])).reshape((-1, 2))
-    
+                
     def move(self, direction, speed):
         self.head = np.add(self.head, np.array([direction[0] * speed[0], direction[1] * speed[1]]))
         last_element = self.body[-1]
@@ -26,10 +26,9 @@ class Snake:
         self.moved_from = last_element
         
     def render(self, BLOCK_WIDTH, BLOCK_HEIGHT, win):
+        pygame.draw.rect(win, (0, 200, 0), (self.head[0], self.head[1], BLOCK_WIDTH, BLOCK_HEIGHT))
+            
         for part in self.body[1:]:
             pygame.draw.rect(win, (0, 255, 0), (part[0], part[1], BLOCK_WIDTH, BLOCK_HEIGHT))
-        
-        pygame.draw.rect(win, (0, 200, 0), (self.head[0], self.head[1], BLOCK_WIDTH, BLOCK_HEIGHT))
-        
-        if (self.moved_from[0] != None and self.moved_from[1] != None):
-            pygame.draw.rect(win, (0, 0, 0), (self.moved_from[0], self.moved_from[1], BLOCK_WIDTH, BLOCK_HEIGHT))
+            
+        pygame.draw.rect(win, (0, 0, 0), (self.moved_from[0], self.moved_from[1], BLOCK_WIDTH, BLOCK_HEIGHT))
