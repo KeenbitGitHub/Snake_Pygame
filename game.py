ca0d1f9dbc1d83106e3293bec978d1292915d749
KeenBitGitHub <KeenBitInfo@gmail.com>
2019-12-28 23:17:30 +0100

    refactored game to a class

--- game.py
+++ game.py
@@ -4,86 +4,95 @@ import food
 import collision_detection
 import numpy as np
 
-def initialize():
-    global WIN_SIZE, WIN_WIDTH, WIN_HEIGHT, CAPTION, game_running, snake_instance, food_instance, collision_detection_instance, win
-    global BLOCK_WIDTH, BLOCK_HEIGHT, BLOCK_SIZE, direction
-    
-    pygame.init()
-    
-    BLOCK_WIDTH, BLOCK_HEIGHT = 50, 50 
-    BLOCK_SIZE = (BLOCK_WIDTH, BLOCK_HEIGHT)
-    
-    WIN_WIDTH, WIN_HEIGHT = 800, 800
-    WIN_SIZE = (WIN_WIDTH, WIN_HEIGHT)
-    win = pygame.display.set_mode(WIN_SIZE)
-    
+class Game:
+    WIN_SIZE = None
+    WIN_WIDTH = None
+    WIN_HEIGHT = None
+    CAPTION = None
+    game_running = None
     snake_instance = None
     food_instance = None
     collision_detection_instance = None
-    direction = (1, 0)
+    win = None
+    BLOCK_WIDTH = None
+    BLOCK_HEIGHT = None
+    BLOCK_SIZE = None
+    direction = None
     
-    snake_instance = snake.Snake((WIN_WIDTH/2, WIN_HEIGHT/2), 4, BLOCK_SIZE)
-    food_instance = food.Food(snake_instance.body, WIN_SIZE, BLOCK_WIDTH, BLOCK_HEIGHT)
-    collision_detection_instance = collision_detection.collision_detector()
+    def __init__(self):
+        self.main()
     
-    CAPTION = "AI plays snake"
-    pygame.display.set_caption(CAPTION)
-    
-    game_running = True
-    
-    
-def player_input():
-    global game_running, direction, BLOCK_SIZE
-    UPDATE_SPEED = 400
-    UPDATE_MOVE = False
-    keys = pygame.key.get_pressed()
-    for event in pygame.event.get():
-        if (event.type == pygame.QUIT):
-            game_running = False
-            UPDATE_SPEED = 0
-            break
-        elif (event.type == pygame.KEYDOWN):
-            if (event.key == pygame.K_LEFT and direction != (1, 0) and not UPDATE_MOVE):
-                direction = (-1, 0)
-                UPDATE_MOVE = True
-            elif (event.key == pygame.K_RIGHT and direction != (-1, 0) and not UPDATE_MOVE):
-                direction = (1, 0)
-                UPDATE_MOVE = True
-            elif (event.key == pygame.K_DOWN and direction != (0, -1) and not UPDATE_MOVE):
-                direction = (0, 1)
-                UPDATE_MOVE = True
-            elif (event.key == pygame.K_UP and direction != (0, 1) and not UPDATE_MOVE):
-                direction = (0, -1)
-                UPDATE_MOVE = True
-    
-    snake_instance.move(direction, BLOCK_SIZE)
-    pygame.time.delay(UPDATE_SPEED)
-    
-    
-def render():
-    global BLOCK_WIDTH, BLOCK_HEIGHT, snake_instance, win
-    food_instance.render(BLOCK_WIDTH, BLOCK_HEIGHT, win)
-    snake_instance.render(BLOCK_WIDTH, BLOCK_HEIGHT, win)
-    pygame.display.update()
-    
-def collision():
-    global snake_instance, food_instance, WIN_SIZE, BLOCK_SIZE
-    col_result = collision_detection_instance.check_collision(snake_instance.head, snake_instance.body, WIN_SIZE, BLOCK_SIZE, food_instance)
-    
-    if (col_result == 1):
-        snake_instance.alive = False
-    elif (col_result == 2):
-        food_instance.respawn(snake_instance.body, WIN_SIZE, BLOCK_SIZE[0], BLOCK_SIZE[1])
-        snake_instance.body = np.append(snake_instance.body, snake_instance.moved_from).reshape((-1, 2))
+    def initialize(self):
+        pygame.init()
+        
+        self.BLOCK_WIDTH, self.BLOCK_HEIGHT = 50, 50 
+        self.BLOCK_SIZE = (self.BLOCK_WIDTH, self.BLOCK_HEIGHT)
+        
+        self.WIN_WIDTH, self.WIN_HEIGHT = 800, 800
+        self.WIN_SIZE = (self.WIN_WIDTH, self.WIN_HEIGHT)
+        self.win = pygame.display.set_mode(self.WIN_SIZE)
+        
+        self.snake_instance = None
+        self.food_instance = None
+        self.collision_detection_instance = None
+        self.direction = (1, 0)
+        
+        self.snake_instance = snake.Snake((self.WIN_WIDTH/2, self.WIN_HEIGHT/2), 4, self.BLOCK_SIZE)
+        self.food_instance = food.Food(self.snake_instance.body, self.WIN_SIZE, self.BLOCK_WIDTH, self.BLOCK_HEIGHT)
+        self.collision_detection_instance = collision_detection.collision_detector()
+        
+        self.CAPTION = "AI plays snake"
+        pygame.display.set_caption(self.CAPTION)
+        
+        self.game_running = True
+        
+        
+    def player_input(self):
+        UPDATE_SPEED = 200
+        update_move = False
+        keys = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if (event.type == pygame.QUIT):
+                self.game_running = False
+                UPDATE_SPEED = 0
+                break
+            elif (event.type == pygame.KEYDOWN):
+                if (event.key == pygame.K_LEFT and self.direction != (1, 0) and not update_move):
+                    self.direction = (-1, 0)
+                    update_move = True
+                elif (event.key == pygame.K_RIGHT and self.direction != (-1, 0) and not update_move):
+                    self.direction = (1, 0)
+                    update_move = True
+                elif (event.key == pygame.K_DOWN and self.direction != (0, -1) and not update_move):
+                    self.direction = (0, 1)
+                    update_move = True
+                elif (event.key == pygame.K_UP and self.direction != (0, 1) and not update_move):
+                    self.direction = (0, -1)
+                    update_move = True
+        
+        self.snake_instance.move(self.direction, self.BLOCK_SIZE)
+        pygame.time.delay(UPDATE_SPEED)
+        
+        
+    def render(self):
+        self.food_instance.render(self.BLOCK_WIDTH, self.BLOCK_HEIGHT, self.win)
+        self.snake_instance.render(self.BLOCK_WIDTH, self.BLOCK_HEIGHT, self.win)
+        pygame.display.update()
+        
+    def collision(self):
+        col_result = self.collision_detection_instance.check_collision(self.snake_instance.head, 
+            self.snake_instance.body, self.WIN_SIZE, self.BLOCK_SIZE, self.food_instance)
+        
+        if (col_result == 1):
+            self.snake_instance.alive = False
+        elif (col_result == 2):
+            self.food_instance.respawn(self.snake_instance.body, self.WIN_SIZE, self.BLOCK_SIZE[0], self.BLOCK_SIZE[1])
+            self.snake_instance.body = np.append(self.snake_instance.body, self.snake_instance.moved_from).reshape((-1, 2))
 
-def main():
-    global game_running, snake_instance
-    
-    initialize()
-    
-    while snake_instance.alive and game_running:
-        player_input()
-        render()
-        collision()
-    
-main()
+    def main(self):
+        self.initialize()
+        
+        while self.snake_instance.alive and self.game_running:
+            self.player_input()
+            self.render()
+            self.collision()
---
+++ main.py
@@ -0,0 +1,3 @@
+import game
+
+game_instance = game.Game()
